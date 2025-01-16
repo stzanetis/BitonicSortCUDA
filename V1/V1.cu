c329e17eff346fa1da89df9bee7f8df2c87e94bd
Savvas Tzanetis <savvastzanetis@gmail.com>
2025-01-16 21:47:52 +0200

    Added .gitignore

--- V1/V1.cu
+++ V1/V1.cu
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <cuda_runtime.h>
 #include <chrono>
-#include "../Utilities/utilities.cuh"
+//#include "../Utilities/utilities.cuh"
+#include <cstdlib>
+#include <ctime>
+
+// Check if the array is sorted
+bool isSorted(int *values, int N) {
+    for (int i = 1; i < N; i++) {
+        if (values[i - 1] > values[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Initialize the array with random integers
+void generateArray(int *values, int N) {
+    std::srand(std::time(0));
+
+    for (int i = 0; i < N; i++) {
+        values[i] = std::rand() % 100; // 0 - 99
+    }
+}
 
 __global__ void bitonicSortKernel(int *dev_values, int N) {
-    unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
+    unsigned int tid = threadIdx.x + blockDim.x * blockIdx.x;
 
     for (unsigned int k = 2; k <= N; k <<= 1) {
         for (unsigned int j = k >> 1; j > 0; j >>= 1) {
@@ -40,6 +61,13 @@ void bitonicSort(int *values, int N) {
     dim3 blocks((N + 511) / 512);
     dim3 threads(512);
 
+    for (int stage = 2; stage <= N; stage <<= 1) {
+        for (int step = stage >> 1; step > 0; step = step >> 1) {
+            bitonicSortStep<<<blocks, threads>>>(dev_values, stage, step);
+            cudaDeviceSynchronize();
+        }
+    }
+
     bitonicSortKernel<<<blocks, threads>>>(dev_values, N);
     cudaDeviceSynchronize();
 
@@ -48,12 +76,13 @@ void bitonicSort(int *values, int N) {
 }
 
 int main(int argc, char *argv[]) {
+    int q, p;
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " {q} {p}" << std::endl;
         return 1;
     } else {
-        int p = std::atoi(argv[1]);
-        int q = std::atoi(argv[2]);
+        p = std::atoi(argv[1]);
+        q = std::atoi(argv[2]);
     }
 
     int N = 1 << (q + p);   // N = 2^(q + p)
